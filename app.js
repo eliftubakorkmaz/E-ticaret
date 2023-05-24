cfb87539ac19966357677c0505c458cd2a66aace
Elif Tuba Korkmaz <126062789+eliftubakorkmaz@users.noreply.github.com>
2023-05-24 10:44:36 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+// !Sepetteki + ve - işlemleri
+
+function arttir(){
+    var sonuc = document.getElementById("sonuc");
+    sonuc.value = Number(sonuc.value) + 1;
+}
+
+function azalt(){
+    var sonuc = document.getElementById("sonuc");
+    sonuc.value = Number(sonuc.value) - 1;
+}
+
+function arttirma(){
+    var sonuc2 = document.getElementById("sonuc2");
+    sonuc2.value = Number(sonuc2.value) + 1;
+}
+
+function azaltma(){
+    var sonuc2 = document.getElementById("sonuc2");
+    sonuc2.value = Number(sonuc2.value) - 1;
+}
+
+function art(){
+    var sonuc3 = document.getElementById("sonuc3");
+    sonuc3.value = Number(sonuc3.value) + 1;
+}
+
+function azal(){
+    var sonuc3 = document.getElementById("sonuc3");
+    sonuc3.value = Number(sonuc3.value) - 1;
+}
